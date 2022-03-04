0ce9d76f383d515456c384059b31c10937484099
Sik0M0D# <98068234+SantiBorgnino@users.noreply.github.com>
2022-03-04 20:37:37 +0000

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Rudimentary Clock Loop
+#while xsetroot -name " `date '+%a %d %b %Y %R' | sed 's/.*,//'` "
+#do
+#    sleep 1
+#done &
+
+# Run programs with exec to kill
+#nitrogen --restore &
+#exec dwmblocks &
+#exec volumeicon &
+#exec nm-applet &
+#exec compton &
+#xsct 3800 &
+
+#mv ~/.config/xsession-errors ~/.config/xsession-errors.old
+#exec dwm > ~/.config/xsession-errors 2>&1
