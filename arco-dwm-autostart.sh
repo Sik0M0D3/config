56ac1e194177d278b34300a3e929c54f68af9dc8
Sik0M0D# <98068234+Sik0M0D3@users.noreply.github.com>
2022-03-30 17:13:39 +0000

    Update and rename auto.sh to arco-dwm-autostart.sh

---
+++ arco-dwm-autostart.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+export XDG_SESSION_DESKTOP='dwm'
+export XDG_CURRENT_DESKTOP='dwm'
+
+function run {
+    if ! pgrep $1 ;
+    then
+        $@&
+    fi
+}
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+# Run One-Shot applications here
+xwallpaper --zoom /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+gammastep -x -m randr && gammastep -O 5000 -m randr &
+
+# Run utility/background applications at dwm start
+numlockx on &
+/usr/bin/pipewire &
+/usr/bin/pipewire-pulse &
+/usr/bin/pipewire-media-session &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Run user applications at boot time
+run xfce4-power-manager &
+run volumeicon &
+run nm-applet &
+run sxhkd &
+picom &
--- auto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run programs with exec to kill
-#nitrogen --restore &
-#exec compton &
-#xsct 3800
-
-#mv ~/.config/xsession-errors ~/.config/xsession-errors.old
-#exec dwm > ~/.config/xsession-errors 2>&1
